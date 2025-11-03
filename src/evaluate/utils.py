efdb99e21bafc40fa067efeba31086b91bbeae4b
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-11-02 20:39:43 -0800

    improve is_endgame

--- src/evaluate/utils.py
+++ src/evaluate/utils.py
@@ -7,11 +7,19 @@ def is_endgame(board: Board) -> bool:
     Simple heuristic: 4 or fewer major/minor pieces (non-pawn, non-king) on the board.
     """
     major_minor_count = 0
-    for rank in range(8):
-        for file in range(8):
-            piece = board.get_piece(rank, file)
-            if piece and piece.piece_type not in (PieceType.PAWN, PieceType.KING):
-                major_minor_count += 1
-                if major_minor_count > 4:
-                    return False
+
+    # Count white major/minor pieces
+    for _, _, piece in board.white_pieces:
+        if piece.piece_type not in (PieceType.PAWN, PieceType.KING):
+            major_minor_count += 1
+            if major_minor_count > 4:
+                return False
+
+    # Count black major/minor pieces
+    for _, _, piece in board.black_pieces:
+        if piece.piece_type not in (PieceType.PAWN, PieceType.KING):
+            major_minor_count += 1
+            if major_minor_count > 4:
+                return False
+
     return major_minor_count <= 4
